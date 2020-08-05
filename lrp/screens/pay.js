53d8b2d2b557cb98a2c3022f63dc70eea5c91e79
Shabrina Yusri <s.yusri@student.unsw.edu.au>
2020-08-05 11:00:36 +1000

    auto circle and recent payments added

--- lrp/screens/pay.js
+++ lrp/screens/pay.js
@@ -3,34 +3,36 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-// Recent Payments
-// function OptionsPayment() {
-// 	const [people, setPeople] = useState([
-// 		{ name: ' UNSW High Street to Central Chalmers Street', key: '1' },
-// 		{ name: ' UNSW High Street to Central Chalmers Street', key: '2' },
-// 		{ name: ' UNSW High Street to Central Chalmers Street', key: '3' },
-// 		{ name: ' UNSW High Street to Central Chalmers Street', key: '4' },
-// 		{ name: ' UNSW High Street to Central Chalmers Street', key: '5' },
-// 		{ name: ' UNSW High Street to Central Chalmers Street', key: '6' },
-// 	])
-
-// 	<ScrollView>
-// 		{ people.map(item => (
-// 			<View key={item.key}>
-// 				<Text style={styles.item}>{item.name}</Text>
-// 			</View>
-// 		))}
-// 	</ScrollView>
-// }
+// import { AppLoading } from 'expo';
+// import {
+//   useFonts,
+//   Fredoka_400Regular,
+// } from '@expo-google-fonts/Fredoka-One';
 
 const Stack = createStackNavigator();
 
-function OptionsMain({ navigation }) {
+function PAY({ navigation }) {
+  
   return (
     <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
-      <View style={historyStyles.historyContainer}>
+      {/* AUTO PAYMENT BUTTON */}
+      <View style={autoStyles.circle}>
+        <Text style={autoStyles.header}>AUTO</Text>
+      </View>
+
+      {/* Text */}
+      <View style={styles.container}>
+          {/* Logo Container */}
+          <Text style={styles.body}>
+            Automatic payments have been set up for this account. 
+          </Text>
+          <Text style={styles.body}>
+            Simply board a light rail carriage and you will be tapped on automatically.
+          </Text>
+      </View>
+
       {/* RECENT PAYMENTS */}
+      <View style={historyStyles.historyContainer}>
         <Text style={historyStyles.header}>RECENT PAYMENTS</Text>
         <View style={tripStyles.tripContainer}>
           <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 15 July 2020</Text>
@@ -48,23 +50,54 @@ function OptionsMain({ navigation }) {
           <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 12 July 2020</Text>
           <Text style={tripStyles.tripPrice}>-$2.99</Text>
         </View>
-        <View style={tripStyles.tripContainer}>
-          <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 11 July 2020</Text>
-          <Text style={tripStyles.tripPrice}>-$2.99</Text>
-        </View>
-        <View style={tripStyles.tripContainer}>
-          <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 10 July 2020</Text>
-          <Text style={tripStyles.tripPrice}>-$2.99</Text>
-        </View>
       </View>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Payment History')}>
         <Text style={styles.button}>SEE ALL</Text>
       </TouchableOpacity>
     </ScrollView>
   );
 }
 
+function OptionsPayment() {
+
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
+    {/* RECENT PAYMENTS */}
+    <View style={historyStyles.historyContainer}>
+      <Text style={historyStyles.header}>RECENT PAYMENTS</Text>
+      <View style={tripStyles.tripContainer}>
+        <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 15 July 2020</Text>
+        <Text style={tripStyles.tripPrice}>-$2.99</Text>
+      </View>
+      <View style={tripStyles.tripContainer}>
+        <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 14 July 2020</Text>
+        <Text style={tripStyles.tripPrice}>-$2.99</Text>
+      </View>
+      <View style={tripStyles.tripContainer}>
+        <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 13 July 2020</Text>
+        <Text style={tripStyles.tripPrice}>-$2.99</Text>
+      </View>
+      <View style={tripStyles.tripContainer}>
+        <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 12 July 2020</Text>
+        <Text style={tripStyles.tripPrice}>-$2.99</Text>
+      </View>
+      <View style={tripStyles.tripContainer}>
+        <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 11 July 2020</Text>
+        <Text style={tripStyles.tripPrice}>-$2.99</Text>
+      </View>
+      <View style={tripStyles.tripContainer}>
+        <Text style={tripStyles.tripRoute}>UNSW High Street to Central Chalmers Street 10 July 2020</Text>
+        <Text style={tripStyles.tripPrice}>-$2.99</Text>
+      </View>
+    </View>
+
+    </ScrollView>  
+  );
+}
 
 // Screen code
 export default class PayScreen extends React.Component {
@@ -107,9 +140,9 @@ export default class PayScreen extends React.Component {
           }}
             >
 
-          <Stack.Screen name="PAY" component={OptionsMain} />
-          {/* <Stack.Screen name="Change Payment Details" component={OptionsPayment} />
-          <Stack.Screen name="Change Card Type" component={OptionsCardType} />
+          <Stack.Screen name="PAY" component={PAY} />
+          <Stack.Screen name="Payment History" component={OptionsPayment} />
+          {/*<Stack.Screen name="Change Card Type" component={OptionsCardType} />
           <Stack.Screen name="Display & Text Size" component={OptionsDisplay} /> */}
         </Stack.Navigator>
       </NavigationContainer>
@@ -123,14 +156,7 @@ export default class PayScreen extends React.Component {
       
       
 			// <ScrollView>
-      //   <View style={styles.container}>
-      //     {/* Logo Container */}
-      //     <Text style={styles.body}>
-      //       Automatic payments have been set up for this account. 
-      //     </Text>
-      //     <Text style={styles.body}>
-      //       Simply board a light rail carriage and you will be tapped on automatically.
-      //     </Text>
+        
 
       //     <StatusBar style="dark" />
       //     <Text style={styles.header}>RECENT PAYMENTS</Text>
@@ -158,8 +184,10 @@ const styles = StyleSheet.create({
   },
 
 	body: {
-		padding: 20,
-		textAlign: 'center',
+		fontWeight: 'bold',
+    textAlign: 'center',
+    paddingHorizontal: 15,
+    fontSize: 18,
   },
   
   button: {
@@ -168,8 +196,30 @@ const styles = StyleSheet.create({
     fontSize: 24,
     alignSelf: 'flex-end',
     fontWeight: 'bold'
+  },
+});
+
+const autoStyles = StyleSheet.create({
+  circle: {
+    width: 230,
+    height: 230,
+    borderRadius: 230/2,
+    backgroundColor: '#34A032',
+    alignSelf: 'center',
+    borderWidth: 8,
+    borderColor: 'white',
+    justifyContent: "center",
+  },
+
+  header: {
+    fontWeight: 'bold',
+		alignSelf: 'center',
+		fontSize: 40,
+		color: 'white',
+    // fontFamily: 'Fredoka-One', cursive,
   }
-  
+
+
 });
 
 const historyStyles = StyleSheet.create({
@@ -184,7 +234,7 @@ const historyStyles = StyleSheet.create({
 
   header: {
     // flex: 2,n
-		// padding: 20,
+		paddingTop: 20,
 		fontWeight: 'bold',
 		textAlign: 'left',
 		fontSize: 26,
